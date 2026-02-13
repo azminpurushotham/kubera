3c26b900ab36bf8a9786336e4e17ac9e998c5af6
asminpurushotham <asminpurushotham@gmail.com>
2026-02-14 02:29:34 +0530

    updated to latest

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,9 @@ pluginManagement {
         maven("https://jitpack.io") // Add JitPack repository
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
